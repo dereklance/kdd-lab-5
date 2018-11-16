a4767ba8338ed269f28320ee869b18f10daf70ae
Ian Battin <ianbattin0@gmail.com>
2018-11-15 16:03:48 -0800

    Implemented outputing df-idf to files  as well as the Vector class representation

--- Vector.py
+++ Vector.py
@@ -1,11 +1,28 @@
+import math
+
 class Vector:
 
-	def __init__(self, document_term_frequencies, total_term_frequencies):
-		self.word_counts = document_term_frequencies
+	def __init__(self, term_frequencies, total_term_frequencies, document_frequencies):
+		self.term_frequencies = term_frequencies
 		self.total_term_frequencies = total_term_frequencies
+		self.document_frequencies = document_frequencies
 
-	def cosine_similarity(other_vector):
+		self.vector_space = self.compute_vector_space()
+
+	def compute_vector_space(self):
+		vector_space = dict()
+		for term, _ in self.term_frequencies.items():
+			vector_space[term] = self.calculate_tf_idf(term)
+		return vector_space
+
+	def calculate_tf_idf(self, term):
+		df = self.document_frequencies[term]
+		idf = math.log(len(self.total_term_frequencies) / df)
+
+		return df * idf
+
+	def cosine_similarity(self, other_vector):
 		return 0
 
-	def okapi(other_vector):
+	def okapi(self, other_vector):
 		return 1
--- textVectorizer.py
+++ textVectorizer.py
@@ -1,18 +1,30 @@
 import sys
+import pprint
+from Vector import Vector
 from parser import parse_documents
 
 def main():
 	data_directory = sys.argv[1]
 	output_filename = sys.argv[2]
-	stop_words_filen = sys.argv[3]
+	stop_words_filename = sys.argv[3]
 
-	total_term_frequencies, document_term_frequencies, document_frequencies = \
-		parse_documents(data_directory, stop_words_filen)
+	# Parse documents
+	document_term_frequencies, total_term_frequencies, document_frequencies = \
+		parse_documents(data_directory, stop_words_filename)
 
-	print(len(total_term_frequencies))
-	print(len(document_term_frequencies))
-	print(len(document_frequencies))
+	# Construct Vector objects
+	vectors = dict()
+	for document, term_frequencies in document_term_frequencies.items():
+		vectors[document] = Vector(term_frequencies, total_term_frequencies, document_frequencies)
 
+	# Output vector objects to files
+	for document, vector in vectors.items():
+		split_filename = document.split('.')
+		output_filename = split_filename[0] + '-out.' + split_filename[1]
+
+		with open('./output/' + output_filename, 'w') as output:
+			for term, idf_df in vector.vector_space.items():
+				output.write(f'{term} {idf_df}\n')
 
 if __name__ == '__main__':
 	main()
